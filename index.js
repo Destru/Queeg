1f03213807e6ecb3f232416a59b72f534eb3fbb2
Alexander Støver <alexander@cyberpunksocial.club>
2021-05-20 12:11:16 -0700

    Fuck you, Trebek.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config(); 
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fetch = require("node-fetch");
 
 // magic strings 🦄
 const ROLE_SUSPECT = '829420138079846471';
+const TREBEK = '400786664861204481';
 
 // #voight-kampff accounts that are <1 week old
 client.on("guildMemberAdd", member => {
@@ -13,6 +15,15 @@ client.on("guildMemberAdd", member => {
 });
 
 client.on("message", message => {
+  // fuck you, Trebek
+  if(message.author.id === TREBEK && Math.random() < 0.05) {
+    fetch('https://insult.mattbas.org/api/insult.json')
+    .then(response => response.json())
+    .then(data => {
+      message.channel.send(`${data.insult}, <@${TREBEK}>`);
+    });
+  }
+
   if (message.author.bot) return;
   if (message.content.indexOf(process.env.PREFIX) !== 0) return;
 
