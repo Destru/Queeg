a8251b7c6e781ab59216fe60760c82e0f16aabdf
Alexander Støver <alexander@cyberpunksocial.club>
2021-05-21 01:19:21 -0700

    Clean-up some variable names. Provide better Voight-Kampff feedback. Update presence.

--- index.js
+++ index.js
@@ -3,33 +3,37 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fetch = require("node-fetch");
 
-const HELLO_WORLD = '844871570211995678';
-const SUSPECT = '829420138079846471';
-const TREBEK = '400786664861204481';
-
 client.on('ready', () => {
   client.user.setPresence({
     status: 'online',
     activity: {
-      name: 'https://twitch.tv/notdestru',
-      type: 'STREAMING'
+      name: 'Better Than Life',
+      type: 'PLAYING'
     }
   });
 });
 
 client.on("guildMemberAdd", member => {
-  fetch(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_TOKEN}&tag=welcome&rating=g`)
-    .then(response => response.json())
-    .then(data => {
-      client.channels.cache.get(HELLO_WORLD).send(data.embed_url);
-    });
+  const channelWelcome = client.channels.cache.get('844871570211995678');
+  const channelSuspect =  client.channels.cache.get('829418613051359292');
+  const roleSuspect = '829420138079846471';
 
   if (Date.now() - member.user.createdAt < 1000 * 60 * 60 * 24 * 7) {
-    member.roles.add(SUSPECT);
+    member.roles.add(roleSuspect);
+    channelWelcome.send(`${member} has been flagged for _Voight-Kampff_.`);
+    channelSuspect.send(`Read the _pinned message_ for more information, ${member}`);
+  } else {
+    fetch(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_TOKEN}&tag=welcome&rating=g`)
+      .then(response => response.json())
+      .then(data => {
+        channelWelcome.send(data.embed_url);
+      });
   }
 });
 
 client.on("message", message => {
+  const TREBEK = '400786664861204481';
+
   if (message.author.id === TREBEK && Math.random() < 0.01) {
     fetch('https://insult.mattbas.org/api/insult.json')
       .then(response => response.json())
