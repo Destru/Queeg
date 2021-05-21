942016e1a266c171372282d5ed77126109791034
Alexander Støver <alexander@cyberpunksocial.club>
2021-05-21 13:52:08 -0700

    Better ping. Make simple polls.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fetch = require("node-fetch");
 
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const ALPHABET = new Array('🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯','🇰','🇱','🇲','🇳','🇴','🇵','🇶','🇷','🇸','🇹','🇺','🇻','🇼','🇽','🇾','🇿');
+const CSC = 'https://cdn.discordapp.com/emojis/837251418247004205.png?v=1';
+
 client.on('ready', () => {
   client.user.setPresence({
     status: 'online',
@@ -47,17 +54,48 @@ client.on('message', message => {
 
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+  const content = args.join(' ');
 
   if (command === 'ping') {
-    message.channel.send('Pong!');
+    message.channel.send(`Pong (${Date.now() - message.createdTimestamp}ms / ${Math.round(client.ws.ping)}ms)`);
   } else 
   if (command === 'giphy') {
-    let tag = encodeURI(args.join(' ') || message.channel.name);
+    let tag = encodeURI(content || message.channel.name);
     fetch(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_TOKEN}&tag=${tag}&rating=g`)
       .then(response => response.json())
       .then(data => {
         message.channel.send(data.data.embed_url);
       });
+  } else
+  if (command === 'poll') {
+    if (content.includes('|')) {
+      const poll = content.split('|');
+      const question = capitalize(poll.shift());
+
+      let description = '';
+      poll.forEach((option, i) => {
+        description = description + `${ALPHABET[i]} ${capitalize(option)}\n`;
+      });
+
+      const embed = new Discord.MessageEmbed()
+        .setTitle(question)
+        .setColor('#ffff00')
+        .setDescription(description)
+        .setFooter('Poll 1.0e', CSC);
+
+      message.channel.send(embed)
+        .then(message => {
+          for (let i = 0; i < poll.length; i++) {
+            message.react(ALPHABET[i]);
+          }
+        });
+    } else {
+      message.channel.send(capitalize(content))
+        .then(message => {
+          message.react('462126280704262144');
+          message.react('462126761098870784');
+        });
+    }
   } else
   if (message.author.id === process.env.ADMIN) {
     if(command === 'message') {
