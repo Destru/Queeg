6a6ce6893c6199dc87ab4943acf2591c281e1aa0
Destru Kaneda <alexander@binaerpilot.no>
2021-08-14 10:22:34 -0700

    Makes  a voter-only command. Allows access to the temple. Praying can now also remove your satanic faith. Horny Jail has more feedback if you're new.

---
+++ commands/community/hailsatan.js
@@ -0,0 +1,19 @@
+const { role } = require('../../config')
+
+module.exports = {
+  name: 'hailsatan',
+  description: `Pledge your allegiance to Satan.`,
+  aliases: ['hail-satan', 'hail'],
+  restricted: 'voter',
+  execute(message) {
+    if (message.member.roles.cache.has(role.satanist)) {
+      message.channel.send(`<:hailsatan:450846760886206464>`)
+    } else {
+      message.member.roles.add(role.satanist)
+      message.channel.send(
+        `You've been granted access to the temple, fellow \`Satanist\`` +
+          `<:satanism:875311775116034049>`
+      )
+    }
+  },
+}
--- commands/community/horny.js
+++ commands/community/horny.js
@@ -4,14 +4,18 @@ const { role } = require('../../config')
 module.exports = {
   name: 'horny',
   description: `I'm so horny, uwu :tongue: :eggplant:`,
+  aliases: ['hornyjail', 'horny-jail'],
   restricted: 'voter',
   execute(message) {
-    message.member.roles.add(role.sinner)
-    message.channel.send(`*bonks ${message.author}*`)
+    if (!message.member.roles.cache.has(role.sinner)) {
+      message.member.roles.add(role.sinner)
+      message.channel.send(`It's straight to <#841057992890646609> for you!`)
+    }
 
     fetch(`https://api.waifu.pics/sfw/bonk`)
       .then((response) => response.json())
       .then((data) => {
+        message.channel.send(`*bonks ${message.author}*`)
         message.channel.send(data.url)
       })
   },
