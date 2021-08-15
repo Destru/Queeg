7242176069bca8cbe184a6ad13578df40da7d4ec
Destru Kaneda <alexander@binaerpilot.no>
2021-08-15 04:54:24 -0700

    Adds a numbers command.

---
+++ commands/apis/number.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+
+module.exports = {
+  name: 'number',
+  description: `Detailed information on a number.`,
+  args: true,
+  example: '42',
+  execute(message, args) {
+    let number = args[0]
+
+    fetch(`http://numbersapi.com/${number}/trivia?json`)
+      .then((response) => response.json())
+      .then((data) => {
+        message.channel.send(data.text)
+      })
+  },
+}
