b4b6197e1d066f193f97819775be22ff8814cae9
Destru Kaneda <alexander@binaerpilot.no>
2021-08-15 04:41:34 -0700

    Let's add some dad jokes for talon.

---
+++ commands/apis/dadjoke.js
@@ -0,0 +1,17 @@
+const { embedColor } = require('../../config')
+const fetch = require('node-fetch')
+
+module.exports = {
+  name: 'dadjoke',
+  description: `Pretty sure Talon wrote all of these.`,
+  execute(message) {
+    fetch(`https://icanhazdadjoke.com/slack`)
+      .then((response) => response.json())
+      .then((data) => {
+        message.channel.send(data.attachments[0].text).then((message) => {
+          message.react('👍')
+          message.react('👎')
+        })
+      })
+  },
+}
