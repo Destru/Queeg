c8a68438276c20e7a5f656bfce3b2bcbca5de305
Alexander Støver <alexander@cyberpunksocial.club>
2021-09-06 13:12:32 -0700

    Allow people to specify a drink.

--- commands/api/drink.js
+++ commands/api/drink.js
@@ -6,10 +6,16 @@ module.exports = {
   name: 'drink',
   description: 'Have a drink.',
   restricted: 'voter',
-  execute(message) {
+  execute(message, args) {
+    const api = 'https://www.thecocktaildb.com/api/json/v1/1/'
     const embed = new Discord.MessageEmbed().setColor(embedColor)
 
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    let url = api
+
+    if (args.length > 0) url += `search.php?s=${encodeURI(args.join(' '))}`
+    else url += 'random.php'
+
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         const drink = data.drinks[0]
@@ -23,11 +29,9 @@ module.exports = {
           .addField('Glass', drink.strGlass, true)
           .addField('Recipe', recipe, true)
           .setTitle(drink.strDrink)
-          .setThumbnail()
 
         message.channel.send(embed).then((message) => {
           message.react('😋')
-          message.react('🤮')
         })
       })
   },
