17c5afa8418b261087142b8c6f2cebbcd15f7794
Alexander Støver <alexander@cyberpunksocial.club>
2023-05-08 02:38:45 -0700

    Testing something...

--- queeg.js
+++ queeg.js
@@ -40,6 +40,6 @@ const http = require('http')
 http
   .createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('QUEEG 500\n')
+    res.end('Queeg\n')
   })
   .listen(8080)
