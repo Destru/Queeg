974373b22dea1770421aa62ca0d5093738235fcf
Alexander Støver <astoever@gmail.com>
2023-12-19 10:20:42 +0100

    Bot cleanup.

--- crontab.js
+++ crontab.js
@@ -70,30 +70,6 @@ const dailyEvents = (client, channel) => {
     })
 }
 
-const dailyMeme = async (client, channel) => {
-  const reddit = 'https://www.reddit.com'
-  const redditImages = 'https://i.redd.it/'
-  const memes = []
-  const subreddits = ['dankleft', 'communismmemes']
-
-  let count = 0
-  subreddits.forEach(async (subreddit) => {
-    const response = await fetch(`${reddit}/r/${subreddit}/top.json?t=today`)
-    const json = await response.json()
-
-    json.data.children.forEach((post) => {
-      if (post.data.url && post.data.url.startsWith(redditImages))
-        memes.push(post.data.url)
-    })
-
-    count++
-    if (count === subreddits.length) {
-      const random = memes[Math.floor(Math.random() * memes.length)]
-      client.channels.cache.get(channel).send(random)
-    }
-  })
-}
-
 const dailyPosts = async (client, channel) => {
   const embed = new Discord.MessageEmbed()
     .setColor('#2f3136')
@@ -155,7 +131,6 @@ module.exports = {
   run: (client) => {
     // dailyDeaths(client, channel.test)
     // dailyEvents(client, channel.test)
-    dailyMeme(client, channel.test)
     dailyPosts(client, channel.test)
   },
 }
